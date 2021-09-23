b6236b5304eb38d0cc98c3565a7997dc602b5281
Tobias Müller <tobimandroid@googlemail.com>
2021-09-23 09:37:38 +0200

    new app

---
+++ myapp/src/App.js
@@ -0,0 +1,45 @@
+// src/App.js
+import './App.css';
+
+function sayhello(){
+  console.log('HELLO')
+}
+
+
+function App() {
+
+  sayhello()
+
+  return (
+
+
+
+    
+    <div className="App">
+    
+    <img src="./logo512.png"></img>
+
+    <h1>Say Hello to ReactJS</h1>
+    <h2>You will learn how to use the most popular
+      frontend Library and nbecome a super ninja Develiper.
+    </h2>
+
+    <button>AWESOME!  </button>
+
+
+  
+
+    </div>
+
+
+    
+
+
+
+
+
+
+  );
+  
+}
+export default App;
